5e1c0bc196dd200af27174fe568ce0b95979dde0
jeromevs <56254178+jeromevs@users.noreply.github.com>
2019-12-20 18:16:14 +0100

    first styling TO DO

--- src/components/TypeGood.js
+++ src/components/TypeGood.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ProgBar from "./ProgBar";
 
 const TypeGood = ({ counter, setCounter, userProject, setUserProject }) => {
@@ -7,21 +7,32 @@ const TypeGood = ({ counter, setCounter, userProject, setUserProject }) => {
       <div className="title">TYPE DE BIEN</div>
       <div className="button-display">
         <span
-          className="typegood-button"
+          className={
+            userProject.typeGood === "maison"
+              ? "typegood-button-on"
+              : "typegood-button-off"
+          }
+          style={{ width: "390px" }}
           onClick={() => {
             setUserProject({ ...userProject, typeGood: "maison" });
             setCounter(counter + 1);
           }}
         >
+          <input type="radio" />
           MAISON
         </span>
         <span
-          className="typegood-button"
+          className={
+            userProject.typeGood === "appartement"
+              ? "typegood-button-on"
+              : "typegood-button-off"
+          }
           onClick={() => {
             setUserProject({ ...userProject, typeGood: "appartement" });
             setCounter(counter + 1);
           }}
         >
+          <input type="radio" />
           APPARTEMENT
         </span>
       </div>
